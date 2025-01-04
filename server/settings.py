dba6113e523f8c4f1f9516f592e373285311ce30
Robbert Korving <r.korving@gmail.com>
2025-01-04 16:06:38 +0100

    applied dev server settings

--- server/settings.py
+++ server/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "server.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "PA001_dev",
+        "USER": "devuser",
+        "PASSWORD": "devpassword",
+        "HOST": "127.0.0.1",
+        "PORT": "54321",
+    }
 }
 
 
